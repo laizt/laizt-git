7d1b02ab423cda7082ee665999eaac8e6ce22e61
laizt <1070718017@qq.com>
2020-02-14 20:10:21 +0800

    v5.0

--- src/main/java/com/laizt/git/gittest/controller/TestGit.java
+++ src/main/java/com/laizt/git/gittest/controller/TestGit.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestGit {
     @RequestMapping("hello")
     public String testGit(){
-        return "hello git adddev adddev moddev 1111" ;
+        return "hello git adddev adddev moddev 1111 33333" ;
     }
 }
