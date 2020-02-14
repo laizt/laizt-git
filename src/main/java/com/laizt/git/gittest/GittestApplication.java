162b0177ee7c75f48d929d5c7b158674df46be12
laizt <1070718017@qq.com>
2020-02-14 13:14:58 +0800

    new git

---
+++ src/main/java/com/laizt/git/gittest/GittestApplication.java
@@ -0,0 +1,14 @@
+package com.laizt.git.gittest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GittestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GittestApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/laizt/git/gittest/controller/TestGit.java
@@ -0,0 +1,12 @@
+package com.laizt.git.gittest.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/")
+public class TestGit {
+    @RequestMapping("hello")
+    public String testGit(){
+        return "hello git";
+    }
+}
